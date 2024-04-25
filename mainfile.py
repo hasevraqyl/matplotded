2bb17a986f27a6b164a0fb9e4c3f54168e4633c8
hasevraqyl <diseserdyuk@mail.ru>
2024-04-25 19:24:14 +0300

    the big deal

---
+++ mainfile.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+x3 = int(input())
+y3 = int(input())
+ya = [y1, y2, y3]
+xa = [x1, x2, x3]
+plt.plot(xa, ya)
+plt.show
